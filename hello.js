9389485606450e2dffb9a5e9035e18213eb40989
anperezp <tomoyocmt@hotmail.com>
2018-05-10 19:51:17 -0500

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function(req,res){
+  res.writtenHead(200,{'Content type':'text/plain'});
+  res.end('Hello Travis!\n')
+  }).listen(1337,'127.0.0.1');
+  console.log('Server running');
